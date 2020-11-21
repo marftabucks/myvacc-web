00e4f5d8e3766760827059b2d0a77e3bce07e9ec
Mario Adriel <marioadrieel@gmail.com>
2020-11-21 22:36:59 +0700

    Add and style home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,79 @@
 @extends('layout')
 
+@section('meta-title')
+    MyVacc
+@endsection
+
 @section('content')
-    INI HOME YAK dari kami anak baik baik
-@stop
+<div class="home">
+    <section>
+        <div class="media-container">
+            <img src="https://via.placeholder.com/400" alt="" class="media media-left width-30">
+            <div class="media__content">
+                <h1 class="heading-1 margin-bottom-s">MyVacc</h1>
+                <p class="text-regular margin-bottom-m">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit nostrum id quos magnam facilis obcaecati earum possimus consectetur, quis laboriosam quibusdam, officia deserunt deleniti illo.</p>
+                <a href="#" class="cta cta--white margin-bottom-xs">Log in</a>
+                <p class="text-small">Don't have an account? <a href="#" class="link text-small">Register Now</a></p>
+            </div>
+        </div>
+    </section>
+
+    <section class="home-facts">
+        <h1 class="heading-1 margin-bottom-xl">Coronavirus Facts</h1>
+
+        <div class="card-wrapper--flex">
+            <div class="card card--m card--borderless">
+                <img src="https://via.placeholder.com/150" alt="" class="card__image card__image--m">
+                <div class="card__content">
+                    <p class="text-regular">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum voluptas perspiciatis vitae, ab sit officia?</p>
+                </div>
+            </div>
+            <div class="card card--m card--borderless">
+                <img src="https://via.placeholder.com/150" alt="" class="card__image card__image--m">
+                <div class="card__content">
+                    <p class="text-regular">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum voluptas perspiciatis vitae, ab sit officia?</p>
+                </div>
+            </div>
+            <div class="card card--m card--borderless">
+                <img src="https://via.placeholder.com/150" alt="" class="card__image card__image--m">
+                <div class="card__content">
+                    <p class="text-regular">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum voluptas perspiciatis vitae, ab sit officia?</p>
+                </div>
+            </div>
+            <div class="card card--m card--borderless">
+                <img src="https://via.placeholder.com/150" alt="" class="card__image card__image--m">
+                <div class="card__content">
+                    <p class="text-regular">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum voluptas perspiciatis vitae, ab sit officia?</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="home-reasons">
+        <h1 class="heading-1 margin-bottom-xl">Coronavirus Facts</h1>
+
+        <div class="card-wrapper--flex margin-bottom-xl">
+            <div class="card card--l card--borderless">
+                <img src="https://via.placeholder.com/250" alt="" class="card__image card__image--l">
+                <div class="card__content">
+                    <p class="text-regular">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum voluptas perspiciatis vitae, ab sit officia?</p>
+                </div>
+            </div>
+            <div class="card card--l card--borderless">
+                <img src="https://via.placeholder.com/250" alt="" class="card__image card__image--l">
+                <div class="card__content">
+                    <p class="text-regular">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum voluptas perspiciatis vitae, ab sit officia?</p>
+                </div>
+            </div>
+            <div class="card card--l card--borderless">
+                <img src="https://via.placeholder.com/250" alt="" class="card__image card__image--l">
+                <div class="card__content">
+                    <p class="text-regular">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum voluptas perspiciatis vitae, ab sit officia?</p>
+                </div>
+            </div>
+        </div>
+
+        <a href="#" class="cta cta--large cta--white">Register Now</a>
+    </section>
+</div>
+@endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,7 +1,55 @@
 <html>
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <title>
+            @yield('meta-title')
+        </title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
+
+        <!-- Stylesheets -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+    
+        <!-- Scripts -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    </head>
     <body>
-        <h1>VACC-U</h1>
+        <nav class="header">
+            <div class="header__link-box">
+                <a href="/" class="logo-box">
+                    <!-- <img src="" alt="" class="logo"> -->
+                    <p class="text-bold text-white text-uppercase">MyVacc</p>
+                </a>
+                <a href="#" class="link-item"><p class="text-bold text-black">Home</p></a>
+                <a href="#" class="link-item"><p class="text-bold text-black">Contact Us</p></a>
+            </div>
+            <div class="header__util">
+                <a href="/login" class="link-item"><p class="text-bold text-black">Login</p></a>
+            </div>
+        </nav>
 
         @yield('content')
+
+        <footer class="footer">
+            <div class="logo-box">
+                <!-- <img src="" alt="" class="logo"> -->
+                <p class="text-bold text-white text-uppercase">MyVacc</p>
+            </div>
+            <div class="header__link-box">
+                <a href="#" class="link-item"><p class="text-regular text-grey">Home</p></a>
+                <a href="#" class="link-item"><p class="text-regular text-grey">Contact Us</p></a>
+            </div>
+            <div class="header__util">
+                <p class="text-regular text-grey">&#169;2020-Form | All Rights Reserved</p>
+            </div>
+        </footer>
     </body>
 </html>
