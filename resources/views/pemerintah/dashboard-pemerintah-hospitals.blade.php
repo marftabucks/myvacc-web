e0f90fc090d9203a5e43aceb64b6d9388ceb5631
Mario Adriel <marioadrieel@gmail.com>
2020-11-23 00:03:38 +0700

    Add and style pemerintah dashboard

--- resources/views/pemerintah/dashboard-pemerintah-hospitals.blade.php
+++ resources/views/pemerintah/dashboard-pemerintah-hospitals.blade.php
@@ -1,5 +1,39 @@
 @extends('layout')
 
+@section('meta-title')
+    MyVacc | Dashboard (Hospitals)
+@endsection
+
 @section('content')
-    Dashboard Pemerintah-Hospitals
-@stop
+<div class="dashboard">
+    <h1 class="heading-1 margin-bottom-xl">Dashboard</h1>
+
+    <div class="tab">
+        <a href="/dashboard-pemerintah-patients" class="tab-link">Patients</a>
+        <a href="#" class="tab-link active">Hospitals</a>
+    </div>
+
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">Hospital Name</th>
+                <th scope="col">Province</th>
+                <th scope="col">City / Regency</th>
+                <th scope="col">Address</th>
+                <th scope="col">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            @for ($i = 0; $i < 10; $i++)
+            <tr>
+                <td>JIH</td>
+                <td>Special Region of Yogyakarta</td>
+                <td>Sleman Regency</td>
+                <td>Ringroad Street</td>
+                <td><a href="/edit-hospital" class="button button--green">Edit</a></td>
+            </tr>
+            @endfor
+        </tbody>
+    </table>
+</div>
+@endsection
--- resources/views/pemerintah/dashboard-pemerintah-patients.blade.php
+++ resources/views/pemerintah/dashboard-pemerintah-patients.blade.php
@@ -1,5 +1,51 @@
 @extends('layout')
 
+@section('meta-title')
+    MyVacc | Dashboard (Patients)
+@endsection
+
 @section('content')
-    Dashboard Pemerintah-Patients
-@stop
+<div class="dashboard">
+    <h1 class="heading-1 margin-bottom-xl">Dashboard</h1>
+
+    <div class="tab">
+        <a href="#" class="tab-link active">Patients</a>
+        <a href="/dashboard-pemerintah-hospitals" class="tab-link">Hospitals</a>
+    </div>
+
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">Full Name</th>
+                <th scope="col">NIK</th>
+                <th scope="col">E-mail</th>
+                <th scope="col">Photo</th>
+                <th scope="col">Status</th>
+                <th scope="col">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            @for ($i = 0; $i < 3; $i++)
+            <tr>
+                <td>Setyawan Putra</td>
+                <td>234235</td>
+                <td>sputra@gmail.com</td>
+                <td><button class="button">View Photo</button></td>
+                <td>Unverified</td>
+                <td><form action=""><button class="button button--green">Verify</button></form></td>
+            </tr>
+            @endfor
+            @for ($i = 0; $i < 9; $i++)
+            <tr>
+                <td>Setyawan Putra</td>
+                <td>234235</td>
+                <td>sputra@gmail.com</td>
+                <td><button class="button">View Photo</button></td>
+                <td>Verified</td>
+                <td><form action=""><button class="button button--green" disabled>Verify</button></form></td>
+            </tr>
+            @endfor
+        </tbody>
+    </table>
+</div>
+@endsection
