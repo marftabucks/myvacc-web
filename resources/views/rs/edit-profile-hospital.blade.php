74188a7e8f58cc358d91bf3189098d6fcbcf81e4
Mario Adriel <marioadrieel@gmail.com>
2020-11-22 22:45:04 +0700

    Fix hospital pages

--- resources/views/rs/edit-profile-hospital.blade.php
+++ resources/views/rs/edit-profile-hospital.blade.php
@@ -18,41 +18,25 @@
 
             <div class="form__input">
                 <p class="text-regular">Daily Quota</p>
-                <input type="text" class="input" name="Daily-quota" required>
+                <input type="text" class="input" name="quota" required>
             </div>
 
             <div class="form__input">
                 <p class="text-regular">Working Hours (Open)</p>
-                <select name="work_hours" id="work_hours" class="select" required>
-                    <option value="" selected hidden>Open Time</option>
-                    <option value="">06:00</option>
-                    <option value="">07:00</option>
-                    <option value="">08:00</option>
-                    <option value="">09:00</option>
-                    <option value="">10:00</option>
-                    <option value="">24 Hours</option>
-                </select>
+                <input type="time" name="time" id="time" class="input">
             </div>
             
             <div class="form__input">
                 <p class="text-regular">Working Hours (Close)</p>
-                <select name="work_hours" id="work_hours" class="select" required>
-                    <option value="" selected hidden>Close Time</option>
-                    <option value="">20:00</option>
-                    <option value="">21:00</option>
-                    <option value="">22:00</option>
-                    <option value="">23:00</option>
-                    <option value="">00:00</option>
-                    <option value="">01:00</option>
-                </select>
+                <input type="time" name="time" id="time" class="input">
             </div>
             
             <div class="form__input">
                 <p class="text-regular">Accept Vaccination</p>
-                <select name="acc_vacc" id="acc_vacc" class="select" required>
-                    <option value="" selected hidden>Yes/No</option>
-                    <option value="">Yes</option>
-                    <option value="">No</option>
+                <select name="accept-vacc" class="select" required>
+                    <option selected hidden>Yes/No</option>
+                    <option value="yes">Yes</option>
+                    <option value="no">No</option>
                 </select>
             </div>
 
--- resources/views/rs/profile-hospital.blade.php
+++ resources/views/rs/profile-hospital.blade.php
@@ -6,14 +6,17 @@
 
 @section('content')
 <div class="profile-hospital">
-    <div class="container container--left container--l">
+    <div class="container container--left container--xl">
         <h2 class="heading-2 margin-bottom-xl align-self-center">Hospital Profile</h1>
 
-        <div class="profile-hospital__subcontainer">
-            <p class="text-bold">Hospital Name</p>
-            <p class="text-regular">Rumah Sakit Cinta Kasih Yesus</p>
+        <div class="profile-hospital__flex-container">
+            <div class="profile-hospital__subcontainer">
+                <p class="text-bold">Hospital Name</p>
+                <p class="text-regular">Rumah Sakit Cinta Kasih Yesus</p>
+            </div>
+            <a href="/edit-profile-hospital" class="cta cta--white">Edit Profile</a>
         </div>
-
+        
         <div class="profile-hospital__subcontainer">
             <p class="text-bold">Province</p>
             <p class="text-regular">Aceh</p>
@@ -24,7 +27,12 @@
             <p class="text-regular">Kota Aceh</p>
         </div>
 
-        <div class="profile-hospital__subcontainer margin-bottom-m">
+        <div class="profile-hospital__subcontainer">
+            <p class="text-bold">E-mail</p>
+            <p class="text-regular">rsyesus@gmail.com</p>
+        </div>
+
+        <div class="profile-hospital__subcontainer">
             <p class="text-bold">Daily Quota</p>
             <p class="text-regular">250</p>
         </div>
