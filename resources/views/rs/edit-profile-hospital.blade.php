362342211dc97f98f8f3ba5099a66a17815de58c
unknown <khairulhashfi@gmail.com>
2020-11-22 22:19:52 +0700

    add edit-profile-hospital and profile-hospital, also edit scss file

--- resources/views/rs/edit-profile-hospital.blade.php
+++ resources/views/rs/edit-profile-hospital.blade.php
@@ -1,5 +1,63 @@
 @extends('layout')
 
+@section('meta-title')
+    MyVacc | Edit Profile Hospital
+@endsection
+
 @section('content')
-    Edit Profile Hospital
-@stop
+<div class="edit-hospital">
+    <div class="container container--left container--xl">
+        <h2 class="heading-2 margin-bottom-xl align-self-center">Edit Profile</h1>
+
+        <form action="" class="form">
+
+            <div class="form__input">
+                <p class="text-regular">Password</p>
+                <input type="password" class="input" name="password" required>
+            </div>
+
+            <div class="form__input">
+                <p class="text-regular">Daily Quota</p>
+                <input type="text" class="input" name="Daily-quota" required>
+            </div>
+
+            <div class="form__input">
+                <p class="text-regular">Working Hours (Open)</p>
+                <select name="work_hours" id="work_hours" class="select" required>
+                    <option value="" selected hidden>Open Time</option>
+                    <option value="">06:00</option>
+                    <option value="">07:00</option>
+                    <option value="">08:00</option>
+                    <option value="">09:00</option>
+                    <option value="">10:00</option>
+                    <option value="">24 Hours</option>
+                </select>
+            </div>
+            
+            <div class="form__input">
+                <p class="text-regular">Working Hours (Close)</p>
+                <select name="work_hours" id="work_hours" class="select" required>
+                    <option value="" selected hidden>Close Time</option>
+                    <option value="">20:00</option>
+                    <option value="">21:00</option>
+                    <option value="">22:00</option>
+                    <option value="">23:00</option>
+                    <option value="">00:00</option>
+                    <option value="">01:00</option>
+                </select>
+            </div>
+            
+            <div class="form__input">
+                <p class="text-regular">Accept Vaccination</p>
+                <select name="acc_vacc" id="acc_vacc" class="select" required>
+                    <option value="" selected hidden>Yes/No</option>
+                    <option value="">Yes</option>
+                    <option value="">No</option>
+                </select>
+            </div>
+
+            <input type="submit" value="Save" class="cta cta--secondary margin-bottom-xs">
+        </form>
+    </div>
+</div>
+@endsection
--- resources/views/rs/profile-hospital.blade.php
+++ resources/views/rs/profile-hospital.blade.php
@@ -1,5 +1,38 @@
 @extends('layout')
 
+@section('meta-title')
+    MyVacc | Profile Hospital
+@endsection
+
 @section('content')
-    Profile Hospital
-@stop
+<div class="profile-hospital">
+    <div class="container container--left container--l">
+        <h2 class="heading-2 margin-bottom-xl align-self-center">Hospital Profile</h1>
+
+        <div class="profile-hospital__subcontainer">
+            <p class="text-bold">Hospital Name</p>
+            <p class="text-regular">Rumah Sakit Cinta Kasih Yesus</p>
+        </div>
+
+        <div class="profile-hospital__subcontainer">
+            <p class="text-bold">Province</p>
+            <p class="text-regular">Aceh</p>
+        </div>
+
+        <div class="profile-hospital__subcontainer">
+            <p class="text-bold">City / Regency</p>
+            <p class="text-regular">Kota Aceh</p>
+        </div>
+
+        <div class="profile-hospital__subcontainer margin-bottom-m">
+            <p class="text-bold">Daily Quota</p>
+            <p class="text-regular">250</p>
+        </div>
+
+        <div class="profile-hospital__subcontainer">
+            <p class="text-bold">Working hours</p>
+            <p class="text-regular">09:00 - 21:00</p>
+        </div>
+    </div>
+</div>
+@endsection
