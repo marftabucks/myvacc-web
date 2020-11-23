c49d041b3e3c9ecb3172b4f5b8da4583bc644c93
Mario Adriel <marioadrieel@gmail.com>
2020-11-23 16:21:43 +0700

    Fix minor problems

--- resources/views/contact-us.blade.php
+++ resources/views/contact-us.blade.php
@@ -9,20 +9,20 @@
     <div class="media-container">
         <img src="{{ asset('assets/contact.png') }}" alt="" class="media media-m media-left">
         <div class="media__content">
-            <h1 class="heading-1 margin-bottom-s">Contact Us</h1>
-            <p class="text-regular margin-bottom-m">Feel free to contact us anytime. We will get back to you as soon as possible!</p>
+            <h1 class="heading-1 margin-bottom-xs">Contact Us</h1>
+            <p class="text-regular margin-bottom-l">Feel free to contact us anytime. We will get back to you as soon as possible!</p>
 
-            <div class="heading-1 margin-bottom-s">
+            <div class="margin-bottom-s">
                 <p class="text-bold">E-mail</p>
                 <p class="text-regular">myvacc_sejahtera@gmail.com</p>
             </div>
 
-            <div class="heading-1 margin-bottom-s">
+            <div class="margin-bottom-s">
                 <p class="text-bold">Address</p>
                 <p class="text-regular">Treasury Tower, 5th Floor, Jl.Sudirman, SCBD, Jakarta </p>
             </div>
             
-            <div class="heading-1 margin-bottom-s">
+            <div>
                 <p class="text-bold">Open Hours</p>
                 <p class="text-regular">09.00 - 20.00</p>
             </div>
